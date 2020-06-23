69131a71d155d4007c976e6ea8ace19bab99629d
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-22 21:01:12 -0500

    Method input, python2 and python3

---
+++ functions/function.py
@@ -0,0 +1,9 @@
+# Input Method
+# In Python 2 the method input return number
+# In python 3 the method input return string
+
+x = input("Write the first value: ")
+y = input("Write the second value: ")
+z = int(x) + int(y)
+
+print(z)
