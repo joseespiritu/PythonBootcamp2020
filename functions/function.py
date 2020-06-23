5bf2f19938d56dab21ae6a3e84dad785a3ec5e30
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-22 21:18:21 -0500

    Convertors

--- functions/function.py
+++ functions/function.py
@@ -2,8 +2,20 @@
 # In Python 2 the method input return number
 # In python 3 the method input return string
 
-x = input("Write the first value: ")
+"""x = input("Write the first value: ")
 y = input("Write the second value: ")
 z = int(x) + int(y)
 
-print(z)
+print(z)"""
+
+c = input("Enter the temperature in celcious: ")
+c = int(c)
+f = (9/5)*c + 32
+print(int(f))
+
+m = input("Enter the number of minutes: ")
+s = input("Enter the number of seconds: ")
+h = int(m)/60 + int(s)/3600
+
+print(h)
+
