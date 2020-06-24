6ee7c1cc044df0cbb9f856301b92a41c22a0b1d3
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-24 07:43:15 -0500

    Writing in a file

--- files/file.py
+++ files/file.py
@@ -1,4 +1,17 @@
 # Open files, read a file
-file = open("text.txt", "r")
+"""file = open("text.txt", "r")
 x = file.read()
-print(x)
+print(x)"""
+
+# Writing files
+file = open("test.txt","w")
+file.write("pink\n")
+file.write("pink\n")
+x = ["red", "blue", "green", "black"]
+
+for item in x: # Add elements from list to file
+    file.write(item + "\n")
+file.close()
+file = open("test.txt", "r", encoding="utf-8")
+read = file.read()
+print(read)
