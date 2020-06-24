8377ec8a0f20e28e88142a6b24a7cf888080fadf
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-24 07:47:34 -0500

    Appending

--- files/file.py
+++ files/file.py
@@ -9,9 +9,14 @@ file.write("pink\n")
 file.write("pink\n")
 x = ["red", "blue", "green", "black"]
 
-for item in x: # Add elements from list to file
+for item in x:  # Add elements from list to file
     file.write(item + "\n")
 file.close()
-file = open("test.txt", "r", encoding="utf-8")
+"""file = open("test.txt", "r", encoding="utf-8")
 read = file.read()
-print(read)
+print(read)"""
+
+# Append element
+file = open("test.txt", "a")
+file.write("pink")
+file.close()
