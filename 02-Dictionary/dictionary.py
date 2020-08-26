a6c09468842a2d6e51940e14c33a5baa80e9abe4
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-08-25 20:08:26 -0500

    Word dont exists, some issues, list of words

--- 02-Dictionary/dictionary.py
+++ 02-Dictionary/dictionary.py
@@ -1,10 +1,32 @@
 import json
 
-class Main():
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    else:
+        print("You have enter wrong word, please check!")
+
+
+class Main:
     def __init__(self):
         super().__init__()
-        self.data = json.load(open("data.json"))
-        print(self.data["smog"])
+
+        self.word = input("Enter the  word you want to search: ")
+        self.output = translate(self.word)
+        if type(self.output) == list:
+            for item in self.output:
+                print(item)
+        else:
+            print(self.output)
+
 
 if __name__ == '__main__':
     Main()
